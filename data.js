3ab91cd26c62e2d4bb303bd1fda0dc8396af8c7e
evafrommeltova <62146183+evafrommeltova@users.noreply.github.com>
2021-04-21 15:30:13 +0200

    web_m1

---
+++ data.js
@@ -0,0 +1,702 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ern-kuchyn",
+      "name": "Černá kuchyně",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1283072704282162,
+          "pitch": 0.49442745713366776,
+          "rotation": 0,
+          "target": "15-svtnice"
+        },
+        {
+          "yaw": -1.3520515551366667,
+          "pitch": 0.5976422212559562,
+          "rotation": 0,
+          "target": "7-komora"
+        },
+        {
+          "yaw": 1.7816173880287032,
+          "pitch": 0.55697839281566,
+          "rotation": 0,
+          "target": "9-mlnice"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-exterir---pohled-2",
+      "name": "Exteriér - pohled 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3878865990865972,
+          "pitch": 0.1206917222815278,
+          "rotation": 0,
+          "target": "4-exterir---pohled-1"
+        },
+        {
+          "yaw": -0.7843085158739012,
+          "pitch": 0.027985376856459965,
+          "rotation": 0,
+          "target": "2-exterir---pohled-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-exterir---pohled-3",
+      "name": "Exteriér - pohled 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8396132504150238,
+          "pitch": 0.1647625896521845,
+          "rotation": 0,
+          "target": "3-exterir---pohled-4"
+        },
+        {
+          "yaw": -0.6182708312967371,
+          "pitch": 0.2007311870632531,
+          "rotation": 0,
+          "target": "9-mlnice"
+        },
+        {
+          "yaw": 0.5758394134822957,
+          "pitch": 0.2101157567823968,
+          "rotation": 0,
+          "target": "1-exterir---pohled-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-exterir---pohled-4",
+      "name": "Exteriér - pohled 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0037223419389729173,
+        "pitch": 0,
+        "fov": 1.557758975746875
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6050981756671661,
+          "pitch": 0.19875040038200176,
+          "rotation": 0,
+          "target": "2-exterir---pohled-3"
+        },
+        {
+          "yaw": -0.7323954673149835,
+          "pitch": 0.16310086228255116,
+          "rotation": 0,
+          "target": "4-exterir---pohled-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-exterir---pohled-1",
+      "name": "Exteriér - pohled 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.22420768083311948,
+        "pitch": -0.04030197575048078,
+        "fov": 1.557758975746875
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6719514372921438,
+          "pitch": 0.19258679571217563,
+          "rotation": 0,
+          "target": "1-exterir---pohled-2"
+        },
+        {
+          "yaw": -0.6913449664116467,
+          "pitch": 0.16292901189960496,
+          "rotation": 0,
+          "target": "5-chlv-1"
+        },
+        {
+          "yaw": -0.27326746795758794,
+          "pitch": 0.13964563343169445,
+          "rotation": 0,
+          "target": "6-chlv-2"
+        },
+        {
+          "yaw": 0.006144159067661192,
+          "pitch": 0.07377162362096534,
+          "rotation": 0,
+          "target": "9-mlnice"
+        },
+        {
+          "yaw": 1.33761161957562,
+          "pitch": 0.13352968626254125,
+          "rotation": 0,
+          "target": "3-exterir---pohled-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.056816181701243806,
+          "pitch": 0.3699274413488496,
+          "title": "<p class=\"MsoNormal\">Virtuální prohlídka stavení Kundratice 60<o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\">Model stavení byl vyhotoven dle plánové dokumentace z&nbsp;roku 1943,\nkterou vytvořil Ing. <span style=\"color:black;background:white\">Vodseďálek. Pro\nmodelaci byl použit program SketchUp a pro vytvoření panoramatických snímků\nprogram Lumion.<o:p></o:p></span></p>"
+        }
+      ]
+    },
+    {
+      "id": "5-chlv-1",
+      "name": "Chlév 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.086299454208607,
+          "pitch": 0.48246113107473043,
+          "rotation": 0,
+          "target": "4-exterir---pohled-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-chlv-2",
+      "name": "Chlév 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.703269292245473,
+          "pitch": 0.6241915945366117,
+          "rotation": 0,
+          "target": "4-exterir---pohled-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-komora",
+      "name": "Komora",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.682586541667404,
+          "pitch": 0.6433106739185188,
+          "rotation": 0,
+          "target": "0-ern-kuchyn"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-komora-1-patro",
+      "name": "Komora 1. patro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.1700501254504072,
+        "pitch": 0,
+        "fov": 1.557758975746875
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.49675555088214907,
+          "pitch": 0.6075659152592525,
+          "rotation": 0,
+          "target": "14-sklad"
+        },
+        {
+          "yaw": -0.7088825474832703,
+          "pitch": 0.500772381727316,
+          "rotation": 0,
+          "target": "16-spka"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-mlnice",
+      "name": "Mlýnice",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5953121889912598,
+          "pitch": 0.21715074622137465,
+          "rotation": 0,
+          "target": "4-exterir---pohled-1"
+        },
+        {
+          "yaw": -0.7409746798309591,
+          "pitch": 0.2927953503622298,
+          "rotation": 0,
+          "target": "15-svtnice"
+        },
+        {
+          "yaw": 0.45442739516045805,
+          "pitch": -0.007980693328594413,
+          "rotation": 0,
+          "target": "10-patro-mlnice"
+        },
+        {
+          "yaw": -2.388546901534191,
+          "pitch": 0.40121533324586167,
+          "rotation": 0,
+          "target": "0-ern-kuchyn"
+        },
+        {
+          "yaw": -2.7638838879365064,
+          "pitch": 0.32315087223635963,
+          "rotation": 0,
+          "target": "2-exterir---pohled-3"
+        },
+        {
+          "yaw": -2.0711405333646056,
+          "pitch": 0.41756328032066925,
+          "rotation": 0,
+          "target": "12-pda"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-patro-mlnice",
+      "name": "Patro mlýnice",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.47094601228904764,
+          "pitch": 0.5986652869708831,
+          "rotation": 0,
+          "target": "9-mlnice"
+        },
+        {
+          "yaw": -1.071804098918978,
+          "pitch": 0.38785076055699896,
+          "rotation": 0,
+          "target": "14-sklad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pda-1-patro",
+      "name": "Půda 1. patro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2092719906369176,
+          "pitch": 0.3735152748074988,
+          "rotation": 0,
+          "target": "12-pda"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-pda",
+      "name": "Půda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4686684236292429,
+          "pitch": 0.2870372729782442,
+          "rotation": 0,
+          "target": "9-mlnice"
+        },
+        {
+          "yaw": 0.184409398398234,
+          "pitch": -0.14522635705237263,
+          "rotation": 0,
+          "target": "11-pda-1-patro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-pda-hospodsk-budova",
+      "name": "Půda hospodářská budova",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1061869465430938,
+          "pitch": 0.35522039880515166,
+          "rotation": 0,
+          "target": "14-sklad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-sklad",
+      "name": "Sklad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.573691112951618,
+          "pitch": 0.4146382612443169,
+          "rotation": 0,
+          "target": "8-komora-1-patro"
+        },
+        {
+          "yaw": -1.3827621400604286,
+          "pitch": -0.058780668528747526,
+          "rotation": 0,
+          "target": "13-pda-hospodsk-budova"
+        },
+        {
+          "yaw": 1.4299912949896054,
+          "pitch": 0.6702273524087303,
+          "rotation": 0,
+          "target": "10-patro-mlnice"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-svtnice",
+      "name": "Světnice",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-spka",
+      "name": "Sýpka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
